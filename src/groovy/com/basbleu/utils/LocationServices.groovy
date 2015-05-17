dd98bde9c2523b86987fa2f59223e8a2311dfa07
Tejaswini <basbleu.india@gmail.com>
2015-05-18 01:12:58 +0530

    Added method in LocationServices to get distance between to areas

--- src/groovy/com/basbleu/utils/LocationServices.groovy
+++ src/groovy/com/basbleu/utils/LocationServices.groovy
@@ -6,6 +6,9 @@ import groovy.json.JsonSlurper
 class LocationServices {
     private static final String URL = "http://maps.googleapis.com/maps/api/geocode/json";
 
+    private static final String DISTANCE_URL="https://maps.googleapis.com/maps/api/distancematrix/json";
+    //?origins=Kothrud,Pune&destinations=Shivajinagar,Pune&key=AIzaSyBkne3CvO54qrbej3tDli6mIVtnPv4iGr8"
+    private static final String DISTANCE_API_KEY="AIzaSyBkne3CvO54qrbej3tDli6mIVtnPv4iGr8";
     /*
      * Here the fullAddress String is in format like
      * "address,city,state,zipcode". Here address means "street number + route"
@@ -23,7 +26,7 @@ class LocationServices {
 
                  inputStream.close();
 //         def parsedJson = new JsonSlurper().parseText(response.toString());
-         println("lat = "+ parsedJson.results[0].geometry.location);
+         println("location::"+fullAddress +"   lat= "+ parsedJson.results[0].geometry.location);
         return parsedJson;
 
 
@@ -54,7 +57,23 @@ class LocationServices {
 //
 //
 //    }
+    /**
+     * Returns the distance between two areas in meters.
+     * @param origin
+     * @param destination
+     * @return
+     *        distance in meters
+     */
+    def static getDistance(String origin, String destination) {
+    URL url= new  URL(DISTANCE_URL+"?origins="+origin+"&destinations="+destination+"&key="+DISTANCE_API_KEY);
+        URLConnection conn = url.openConnection();
+                InputStream inputStream = conn.getInputStream() ;
+        def parsedJson = new JsonSlurper().parse(inputStream)
+                inputStream.close();
+        println("distance "+ parsedJson.rows[0].elements[0].distance.value);
+        return parsedJson;
 
 
+    }
 
 }
--- test/unit/com/basbleu/utils/LocationServicesSpec.groovy
+++ test/unit/com/basbleu/utils/LocationServicesSpec.groovy
@@ -6,12 +6,51 @@ class LocationServicesSpec extends Specification{
 
     def "It should give me lat-long of a address"() {
         given:
-          String address = "Kothrud"
-
+        String address1 = "Kothrud"
+       /* String address2 = "Sahakarnagar"
+        String address3 = "Aundh"
+        String address4 = "Baner"
+        String address5 = "Balewadi"
+        String address6 = "Pashan"
+        String address7 = "Vimanagar"
+        String address8 = "Hinjewadi"
+        String address9 = "Wagholi"
+        String address10 = "Shivajinagar"
+        String address11 = "Bhosalenagar"
+        String address12 = "Law College Road"
+        String address13 = "Pimpri"
+        String address14 = "Kondhwa"
+        String address15 = "Katraj"
+      */
         when:
-            def result = new LocationServices().convertToLatLong(address)
-            println(result)
+            def result = new LocationServices().convertToLatLong(address1)
+        /* result = new LocationServices().convertToLatLong(address2)
+         result = new LocationServices().convertToLatLong(address3)
+         result = new LocationServices().convertToLatLong(address4)
+         result = new LocationServices().convertToLatLong(address5)
+         result = new LocationServices().convertToLatLong(address6)
+         result = new LocationServices().convertToLatLong(address7)
+         result = new LocationServices().convertToLatLong(address8)
+         result = new LocationServices().convertToLatLong(address9)
+         result = new LocationServices().convertToLatLong(address10)
+         result = new LocationServices().convertToLatLong(address11)
+         result = new LocationServices().convertToLatLong(address12)
+        result = new LocationServices().convertToLatLong(address13)
+        result = new LocationServices().convertToLatLong(address14)
+        result = new LocationServices().convertToLatLong(address15)
+         */
+
+
         then:
          true
     }
+ def "It should give me the distance between two locations in meters"(){
+  given:
+   String origin="Kothrud,pune"
+  String destination="Shivajinagar,Pune"
+  when:
+  def result= new LocationServices().getDistance(origin,destination);
+  then:
+  true;
+ }
 }
