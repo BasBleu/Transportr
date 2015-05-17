fd33b22058ec9cc517e4fec9f4dd593182054dff
Experiment <basbleu.india@gmail.com>
2015-05-17 18:19:21 +0530

    Db setup script to add vendors to the system

---
+++ dbSetup.js
@@ -0,0 +1,13 @@
+var db = db.getSiblingDB('Transportr');
+
+db.vendors.insert({"name": "Vinayak Kale", "vehicleNo": "MH12 AJ142" ,
+    "phoneNo": "+917798292606", "area": "Katraj", "radius": "90"});
+
+db.vendors.insert({"name": "Asif Khan", "vehicleNo": "MH12 CX8895" ,
+    "phoneNo": "+919565218878", "area": "Kondhwa", "radius": "70"});
+
+db.vendors.insert({"name": "Abhay Lokhande", "vehicleNo": "MH14 J2105" ,
+    "phoneNo": "+918857063264", "area": "Wagholi", "radius": "100"});
+
+db.vendors.insert({"name": "Tejas Sutar", "vehicleNo": "MH12 GR5165" ,
+    "phoneNo": "+917876554269", "area": "Pimpri", "radius": "100"});
