14702254cc4f311012950e63184710c319aaff4f
Experiment <basbleu.india@gmail.com>
2015-05-20 23:03:58 +0530

    Added Area in model and a repository to find a particular area by name

---
+++ grails-app/domain/com/basBleu/model/Area.groovy
@@ -0,0 +1,10 @@
+package com.basBleu.model
+
+/**
+ * Created by experiment on 20/5/15.
+ */
+class Area {
+    String name;
+    String latitude;
+    String longitude;
+}
---
+++ src/groovy/com/basBleu/repositories/LocationRepository.groovy
@@ -0,0 +1,17 @@
+package com.basBleu.repositories
+
+import com.basBleu.model.Area
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+
+class LocationRepository {
+    MongoOperations mongoOperations;
+
+    def getAreaByName(String name) {
+        def criteria = Criteria.where("name").is(name)
+        Query query = new Query(criteria);
+        mongoOperations.findOne(query, Area.class);
+    }
+
+}
